e8e481a802731b59594fb3292b2c3886e2c9db8f
baku <planet.of.early.summer.rain@gmail.com>
2019-09-19 14:59:17 +0900

    chapter7 end

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,20 @@
+  
 import './bootstrap'
 import Vue from 'vue'
-// ルーティングの定義をインポートする
 import router from './router'
-// ルートコンポーネントをインポートする
+import store from './store'
 import App from './App.vue'
 
-import store from './store' // ★　追加
+const createApp = async () => {
+  await store.dispatch('auth/currentUser').catch(() => {})
 
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App />'
+  })
+}
 
-new Vue({
-  el: '#app',
-  router, // ルーティングの定義を読み込む
-  store, // ★　追加
-  components: { App }, // ルートコンポーネントの使用を宣言する
-  template: '<App />' // ルートコンポーネントを描画する
-})
+createApp()
--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -24,19 +24,19 @@
       </form>
     </div>
     <div class="panel" v-show="tab === 2">
-    <form class="form" @submit.prevent="register">
-      <label for="username">Name</label>
-      <input type="text" class="form__item" id="username" v-model="registerForm.name">
-      <label for="email">Email</label>
-      <input type="text" class="form__item" id="email" v-model="registerForm.email">
-      <label for="password">Password</label>
-      <input type="password" class="form__item" id="password" v-model="registerForm.password">
-      <label for="password-confirmation">Password (confirm)</label>
-      <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
-      <div class="form__button">
-        <button type="submit" class="button button--inverse">register</button>
-      </div>
-    </form>
+      <form class="form" @submit.prevent="register">
+        <label for="username">Name</label>
+        <input type="text" class="form__item" id="username" v-model="registerForm.name">
+        <label for="email">Email</label>
+        <input type="text" class="form__item" id="email" v-model="registerForm.email">
+        <label for="password">Password</label>
+        <input type="password" class="form__item" id="password" v-model="registerForm.password">
+        <label for="password-confirmation">Password (confirm)</label>
+        <input type="password" class="form__item" id="password-confirmation" v-model="registerForm.password_confirmation">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">register</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -50,7 +50,7 @@ export default {
         email: '',
         password: ''
       },
-        registerForm: {
+      registerForm: {
         name: '',
         email: '',
         password: '',
@@ -58,20 +58,19 @@ export default {
       }
     }
   },
-    methods: {
-      async login () {
+  methods: {
+    async login () {
       // authストアのloginアクションを呼び出す
       await this.$store.dispatch('auth/login', this.loginForm)
-
       // トップページに移動する
       this.$router.push('/')
     },
-      async register () {
-        // authストアのresigterアクションを呼び出す
-        await this.$store.dispatch('auth/register', this.registerForm)
-        // トップページに移動する
-        this.$router.push('/')
-      }
+    async register () {
+      // authストアのresigterアクションを呼び出す
+      await this.$store.dispatch('auth/register', this.registerForm)
+      // トップページに移動する
+      this.$router.push('/')
     }
+  }
 }
 </script>
